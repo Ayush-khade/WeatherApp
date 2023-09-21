3a7d49719fedf9335a2152719fba75c0b41d11b1
Vaishnavi Bochare <vaishnavibochare31@gmail.com>
2023-09-22 00:42:32 +0530

    add button tag

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Text,View } from "react-native";
+import { Button, Text,View } from "react-native";
 const App=()=>{
   return(
     <View style={{flex:1,backgroundColor:"pink",justifyContent:"center"}}>
       <Text style={{fontSize:25,color:"black",fontWeight:"bold",alignSelf:"center"}}>React-Native</Text>
+      <View style={{marginTop:15,width:"auto",height:"auto"}}>
+      <Button title="Next screen" color={"black"}/>
+      </View>
+
     </View>
 
   )
